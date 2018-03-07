d40bd67304c01bdeeb0196f06f5812b4082ad43e
aycabta <aycabta@gmail.com>
2018-03-08 04:38:37 +0900

    Initial files

---
+++ README.md
@@ -0,0 +1,21 @@
+Netflix Seeker
+==============
+
+Netflix seeker with benry.
+
+[This Greasemonkey script on OpenUserJS.org](https://openuserjs.org/scripts/aycabta/Netflix_Seeker)
+
+## Usage
+
+In Netflix video, you can use some key commands below:
+
+<dl>
+<dt><code>u</code></dt><dd>Seeks to seconds ago (defualt 5 seconds)</dd>
+<dt><code>o</code></dt><dd>Seeks to seconds later (default 5 seconds)</dd>
+<dt><code>[</code></dt><dd>Increments seek duration for <code>u</code> and <code>o</code></dd>
+<dt><code>]</code></dt><dd>Decrements seek duration for <code>u</code> and <code>o</code></dd>
+<dt><code>s</code></dt><dd>Saves present time for seeking with <code>r</code></dd>
+<dt><code>r</code></dt><dd>Seeks to the time what is saved by <code>s</code></dd>
+</dl>
+
+The seek seconds will be shown on next to video duration.
---
+++ netflix-seeker.user.js
@@ -0,0 +1,82 @@
+// ==UserScript==
+// @name     Netflix Seeker
+// @license  MIT
+// @version  0.0.1
+// @description  benry
+// @include    /^https:\/\/www\.netflix\.com\/watch\/.*$/
+// @copyright  2018+, aycabta
+// @grant    none
+// ==/UserScript==
+
+(function() {
+    function initialize() {
+        var savedTime = null;
+        var seekSeconds = 5;
+        var isShownControls = true;
+        var leftControls = document.getElementsByClassName('video-title')[0];
+        var secondsInfo = document.createElement('div');
+        secondsInfo.style.color = '#BBB';
+        secondsInfo.style.fontSize = '2.2em';
+        secondsInfo.classList.add('time-display');
+        secondsInfo.classList.add('ellipsize-text');
+        var secondsText = document.createTextNode('seek ' + seekSeconds.toString(10))
+        secondsInfo.appendChild(secondsText);
+        leftControls.appendChild(secondsInfo);
+        document.addEventListener("keydown", function(e) {
+            var to = null;
+            var videoPlayer = netflix.appContext.state.playerApp.getAPI().videoPlayer;
+            var playerSessionId = videoPlayer.getAllPlayerSessionIds()[0];
+            var player = videoPlayer.getVideoPlayerBySessionId(playerSessionId);
+            switch (e.keyCode) {
+                case 219: // [
+                    seekSeconds--;
+                    console.log("seek with " + seekSeconds + " seconds");
+                    secondsText.textContent = 'seek ' + seekSeconds.toString(10);
+                    break;
+                case 221: // ]
+                    seekSeconds++;
+                    console.log("seek with " + seekSeconds + " seconds");
+                    secondsText.textContent = 'seek ' + seekSeconds.toString(10);
+                    break;
+                case 85: // u
+                    to = player.getCurrentTime() - seekSeconds * 1000;
+                    break;
+                case 79: // o
+                    to = player.getCurrentTime() + seekSeconds * 1000;
+                    break;
+                case 83: // s
+                    savedTime = player.getCurrentTime();
+                    break;
+                case 82: // r
+                    if (savedTime !== null) {
+                        to = savedTime;
+                    }
+                    break;
+                case 73: // i
+                    if (player.getPaused()) {
+                        player.play();
+                    } else {
+                        player.pause();
+                    }
+                    break;
+            }
+            if (to !== null) {
+                if (to < 0) {
+                    to = 0;
+                } else if (to > player.getDuration()) {
+                    to = player.getDuration();
+                }
+                player.seek(to, true);
+            }
+        });
+    };
+    var script = document.createElement ('script');
+    script.textContent = '(' + initialize.toString() + ')()';
+    var head = document.getElementsByTagName ('head')[0];
+    var intervalId = setInterval(function() {
+        if (typeof document.getElementsByClassName('video-title')[0] !== "undefined") {
+            head.appendChild(script);
+            clearInterval(intervalId);
+        }
+    }, 200);
+})();
